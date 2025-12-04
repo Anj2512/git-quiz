faa5623ee066fa7db460f233b8cca9c65db8325b
Sathvika Gokul <30000065@sbstudents.org>
2025-12-04 11:47:43 -0500

    we picked bigger books

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Thread worker = new Thread(() -> {
 
             processFile("warandpeace.txt", "warandpeaceCAP.txt");
+
             processFile("confessionsofstaug.txt", "confessionsofstaugCAP.txt");
 
         });
